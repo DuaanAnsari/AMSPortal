6b6acd4f83a5f9dc61e1337adf96f7f491e90f57
Duaan Ansari <153978127+DuaanAnsari@users.noreply.github.com>
2026-04-13 13:07:42 +0500

    Delete vite-js directory

--- vite-js/src/sections/Supply-Chain/view/index.js
+++
@@ -1,6 +0,0 @@
-export { default as UserEditView } from './user-edit-view';
-export { default as UserListView } from './user-list-view';
-export { default as UserCardsView } from './Quick-Search';
-export { default as MerchandiserBacklogView } from './merchandiser-backlog';
-export { default as UserProfileView } from './My-Order';
-export { default as TNAViewPage } from './TNA-View';
